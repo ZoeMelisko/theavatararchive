88daec00b0c0177ab639e1da55b4e4d0b582fc6b
Zoe Melisko <zoe@zoemelisko.com>
2025-11-10 15:52:28 -0800

    Basic Home done, basic class avatar gallery set up

---
+++ src/components/Title.tsx
@@ -0,0 +1,14 @@
+
+
+function NavBar() {
+
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <p className="textarea-xl">The Avatar Archive</p>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
